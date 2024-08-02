a8e4df4dfb4911e0a8aeb95b200d764093ab7a66
Diogo Realles <diogorealles@hotmail.com>
2024-08-02 03:59:39 -0300

    [FEATURE] Mise en Place...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+const btn = document.querySelector('[data-js="btn"]');
+const yt = document.querySelector('[data-js="yt"]');
+
+btn.addEventListener('click', () => {
+  yt.classList.add('active');
+});
